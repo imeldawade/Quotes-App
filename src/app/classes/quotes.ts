622c7d471af855238b7dd165c73dcfe40ae004c7
imeldawade <imelda.wade@student.moringaschool.com>
2022-02-08 13:18:20 +0300

    quotes constructor

--- src/app/classes/quotes.ts
+++ src/app/classes/quotes.ts
@@ -1,16 +1,16 @@
-// export class Quotes {
+export class Quotes {
 
-//   showInsights!: boolean;
+  showInsights!: boolean;
 
-//   constructor (
-//     public id: number,
-//     public quote: string,
-//     public postBy: string,
-//     public author: string,
-//     public postDate: Date,
-//     public likes: number = 0,
-//     public dislikes: number = 0,
-//   ) {
-//     this.showInsights = false;
-//   }
-// }
+  constructor (
+    public id: number,
+    public quote: string,
+    public postBy: string,
+    public author: string,
+    public postDate: Date,
+    public likes: number = 0,
+    public dislikes: number = 0,
+  ) {
+    this.showInsights = false;
+  }
+}
