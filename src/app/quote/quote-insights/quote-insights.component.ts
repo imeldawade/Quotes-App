630e035efb0515ebce40f8bb2700183cf035b9f8
imeldawade <imelda.wade@student.moringaschool.com>
2022-02-08 13:45:22 +0300

    downvote/upvote

--- src/app/quote/quote-insights/quote-insights.component.ts
+++ src/app/quote/quote-insights/quote-insights.component.ts
@@ -1,34 +1,34 @@
-// import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
-// import { Quotes } from 'src/app/classes/quotes';
+import { Quotes } from 'src/app/classes/quotes';
 
-// @Component({
-//   selector: 'app-quote-insights',
-//   templateUrl: './quote-insights.component.html',
-//   styleUrls: ['./quote-insights.component.css']
-// })
+@Component({
+  selector: 'app-quote-insights',
+  templateUrl: './quote-insights.component.html',
+  styleUrls: ['./quote-insights.component.css']
+})
 
-// export class QuoteInsightsComponent implements OnInit {
+export class QuoteInsightsComponent implements OnInit {
 
-//   @Input() quotin!: Quotes;
+  @Input() quotin!: Quotes;
 
-//   //    Create a new function defined by the @output decorator as an Event Emitter that takes in a boolean
-//   @Output() isDeleted = new EventEmitter<boolean>();
-//   deleteQuote(deleted: boolean){
-//     this.isDeleted.emit(deleted);
-//   }
+  //    Create a new function defined by the @output decorator as an Event Emitter that takes in a boolean
+  @Output() isDeleted = new EventEmitter<boolean>();
+  deleteQuote(deleted: boolean){
+    this.isDeleted.emit(deleted);
+  }
  
-//   //  Declaring the functions to enable us capture the likes and dislikes
-//   likeQuote(){
-//     this.quotin.likes++
-//   }
-//   dislikeQuote(){
-//     this.quotin.dislikes++
-//   }
+  //  Declaring the functions to enable us capture the likes and dislikes
+  likeQuote(){
+    this.quotin.likes++
+  }
+  dislikeQuote(){
+    this.quotin.dislikes++
+  }
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
