e64b5cd18f64895faf53011e3005b74ccdd637a1
imeldawade <imelda.wade@student.moringaschool.com>
2022-02-08 13:53:59 +0300

    For functionality

--- src/app/quote/quote-form/quote-form.component.ts
+++ src/app/quote/quote-form/quote-form.component.ts
@@ -1,24 +1,24 @@
-// import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-// import { Quotes } from 'src/app/classes/quotes';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quotes } from 'src/app/classes/quotes';
 
-// @Component({
-//   selector: 'app-quote-form',
-//   templateUrl: './quote-form.component.html',
-//   styleUrls: ['./quote-form.component.css']
-// })
-// export class QuoteFormComponent implements OnInit {
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
 
-//   newQuote = new Quotes(0, "", "", "", new Date, 0, 0);
+  newQuote = new Quotes(0, "", "", "", new Date, 0, 0);
 
-//   @Output() addQuote = new EventEmitter<Quotes>();
+  @Output() addQuote = new EventEmitter<Quotes>();
 
-//   submitQuote() {
-//     this.addQuote.emit(this.newQuote);
-//   }
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+  }
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
